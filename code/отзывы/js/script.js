48416e9604f701107e10fad65bec9761f9b20c3a
Viktor-Sivak <victorsiv@yandex.ru>
2021-07-22 17:04:59 +0300

    slider

--- code/отзывы/js/script.js
+++ code/отзывы/js/script.js
@@ -2,9 +2,32 @@ $(function () {
 
 $(document).ready(function(){
   $('.reviews-slider').slick({
-    infinite: false,
-    sledesToShow: 4,
+    prevArrow: false,
+    infinite: true,
+    slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+        
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   });
 });
 })
